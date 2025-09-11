f9af71be4d9134f70efd39264d5f9ac014caf3ab
Imran Ahmed <imran.ahk26@gmail.com>
2025-09-11 16:16:06 +0600

    :zap: create app component

---
+++ src/app/app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppComponent from '@/components/app';
+
+const page = () => {
+  return (
+    <div>
+      <AppComponent />
+    </div>
+  );
+};
+
+export default page;
---
+++ src/components/app/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AppComponent = () => {
+  return (
+    <div>
+      <div>App</div>
+    </div>
+  );
+};
+
+export default AppComponent;
